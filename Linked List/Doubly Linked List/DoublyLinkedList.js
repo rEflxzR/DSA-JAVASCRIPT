297dec96e1b25023c83fb5a41778df4f83ef1b36
PRIYANSHU SINGH <60063068+rEflxzR@users.noreply.github.com>
2020-08-07 00:08:13 +0530

    Refactored DLL Class

--- Linked List/Doubly Linked List/DoublyLinkedList.js
+++ Linked List/Doubly Linked List/DoublyLinkedList.js
@@ -13,9 +13,22 @@ class DoublyLinkedList {
         this.tail = null
     }
 
-    push(val) {
+    isEmpty() {
+        return this.head==null && this.tail==null
+    }
+
+    front() {
+        return this.head
+    }
+
+    back() {
+        return this.tail
+    }
+
+    pushBack(val) {
         let newnode = new Node(val)
-        if(!this.head) {
+
+        if(this.isEmpty()) {
             this.head = newnode
             this.tail = newnode
         }
@@ -24,108 +37,113 @@ class DoublyLinkedList {
             newnode.previous = this.tail
             this.tail = newnode
         }
+
         this.length++
     }
 
-    pop() {
-        let newlastnode = this.tail.previous
-        let temp = this.tail
-        newlastnode.next = null
-        temp.previous = null
-        this.tail = newlastnode
-        this.length--
-
-        return temp
-    }
-
-    shift() {
-        let newhead = this.head.next
-        let temp = this.head
-        newhead.previous = null
-        temp.next = null
-        this.head= newhead
-        this.length--
-
-        return temp
-    }
-
-    unshift(val) {
+    pushFront(val) {
         let newnode = new Node(val)
-        this.head.previous = newnode
-        newnode.next = this.head
-        this.head = newnode
+
+        if(this.isEmpty()) {
+            this.head = newnode
+            this.tail = newnode
+        }
+        else {
+            this.head.previous = newnode
+            newnode.next = this.head
+            this.head = newnode
+        }
+
         this.length++
+    }
+
+    popBack() {
+        if(this.isEmpty()) {
+            return "Linked List is Empty"
+        }
+
+        let lastnode = this.tail
+        this.tail = this.tail.previous
+        this.tail.next = null
+        lastnode.previous = null
+        this.length--
+
+        return lastnode
+    }
+
+    popFront() {
+        if(this.isEmpty()) {
+            return "Linked List is Empty"
+        }
+        
+        let headnode = this.head
+        this.head = this.head.next
+        headnode.next = null
+        this.head.previous = null
+        this.length--
+
+        return headnode
     }
 
     get(n) {
-        if(n<0 || n>this.length) {
+        if(n<=0 || n>this.length) {
             return `Invalid Index Provided`
         }
-        else {
-            let i = 1
-            let temp = this.head
-            while(i<n) {
-                temp = temp.next
-                i++
-            }
-            return temp
+
+        let temp = this.head
+        for(let i=1; i<n; i++) {
+            temp = temp.next
         }
+        return temp
     }
 
     set(n, val) {
-        if(n<0 || n>this.length) {
-            console.log("Invalid Index Position") 
+        if(n<=0 || n>this.length) {
+            return `Invalid Index Provided`
         }
-        else {
-            let i=1
-            let temp = this.head
-            while(i<n) {
-                temp = temp.next
-                i++
-            }
-            temp.data = val
+
+        let temp = this.head
+        for(let i=1; i<n; i++) {
+            temp = temp.next
         }
+
+        temp.data = val
     }
 
     insert(n, val) {
-        let newnode = new Node(val)
-        if(n<0 || n>this.length) {
-            console.log(`Invalid Index Position`)
+
+        if(n<=0 || n>this.length) {
+            return `Invalid Index Position`
         }
-        else if(n==1) {
+
+        let newnode = new Node(val)
+        if(n==1) {
             newnode.next = this.head
             this.head.previous = newnode
             this.head = newnode
-            this.length++
-        }
-        else if(n==this.length) {
-            this.tail.next = newnode
-            newnode.previous = this.tail
-            this.tail = newnode
-            this.length++
         }
         else {
-            let i = 1
             let temp = this.head
-            while(i<n) {
+            for(let i=1; i<n-1; i++) {
                 temp = temp.next
-                i++
             }
-            newnode.next = temp
-            temp.previous.next = newnode
-            newnode.previous = temp.previous
-            temp.previous = newnode
-            this.length++
+            newnode.next = temp.next
+            temp.next.previous = newnode
+            temp.next = newnode
+            newnode.previous = temp
         }
+
+        this.length++
     }
 
     remove(n) {
-        let temp = this.head
 
-        if(n<0 || n>this.length) {
+        if(n<=0 || n>this.length) {
             return "Invalid Index Position"
         }
-        else if(n==1) {
+
+        let temp = this.head
+        if(n==1) {
             this.head = temp.next
             this.head.previous = null
             temp.next = null
@@ -137,10 +155,8 @@ class DoublyLinkedList {
             temp.previous = null
         }
         else {
-            let i=1
-            while(i<n) {
+            for(let i=1; i<n; i++) {
                 temp = temp.next
-                i++
             }
             temp.previous.next = temp.next
             temp.next.previous = temp.previous
@@ -153,12 +169,12 @@ class DoublyLinkedList {
     }
 
     print() {
-        let temp = this.head
-        if(!this.head) {
-            console.log("Linked List is Empty")
+        if(this.isEmpty()) {
+            console.log(`Linked List is Empty`)
         }
         else {
-            while(temp != null) {
+            let temp = this.head
+            while(temp!=null) {
                 console.log(temp.data)
                 temp = temp.next
             }
@@ -166,45 +182,5 @@ class DoublyLinkedList {
     }
 }
 
-let list = new DoublyLinkedList()
-list.print()
-console.log("\n--------Push Operation--------\n")
-list.push("ReflxzR")
-list.push("jAnzWE")
-list.push("rEplan")
-list.push("qLimAxz")
-list.push("PC")
-list.push("kEX")
-list.print()
-console.log("\n")
-console.log("\n--------Pop Operation--------\n")
-console.log(list.pop())
-console.log("\n")
-console.log(list)
-console.log("\n--------Shift Operation--------\n")
-console.log(list.shift())
-console.log("\n")
-console.log(list)
-console.log("\n--------Unshift Operation--------\n")
-list.unshift("rEflxzR")
-list.print()
-console.log("\n--------Get Operation--------\n")
-list.print()
-console.log("\n")
-console.log(list.get(3))
-console.log(list.get(1))
-console.log(list.get(4))
-console.log(list.get(7))
-console.log("\n--------Set Operation--------")
-list.set(1, "ReflxzR")
-console.log("\n")
-list.print()
-console.log("\n--------Insert Operation--------\n")
-list.insert(4, "Phantasy")
-list.print()
-console.log("\n--------Remove Operation--------")
-console.log(list.remove(2))
-console.log("\n")
-console.log(list)
-console.log("\n")
-list.print()
+module.exports.Node = Node
+module.exports.DoublyLinkedList = DoublyLinkedList
