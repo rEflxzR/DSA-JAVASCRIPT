25de6911c5b3006565b38fdfa81d5a2575526b79
PRIYANSHU SINGH <60063068+rEflxzR@users.noreply.github.com>
2020-08-10 00:35:49 +0530

    Add files via upload

---
+++ Stacks/Next Smaller Element.js
@@ -0,0 +1,27 @@
+let {Stack} = require('./Stack.js')
+
+function nextSmallerElement(array) {
+
+    let stack = new Stack()
+    let arr = []
+
+    stack.push(array[array.length-1])
+    arr.push(-1)
+
+    for(let i=array.length-2; i>=0; i--) {
+        while( !stack.isEmpty() && array[i] <= stack.top().data ) {
+            stack.pop()
+        }
+
+        stack.isEmpty() ? arr.push(-1) : arr.push(stack.top().data)
+        stack.push(array[i])
+    }
+
+    arr.reverse()
+    console.log(arr)
+}
+
+
+nextSmallerElement([90, 86, 66, 53, 35, 23])
+nextSmallerElement([99, 186, 366, 553, 735, 923])
+nextSmallerElement([384, 7833,234, 598, 25, 123])
